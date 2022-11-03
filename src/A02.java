90065bcdc84ee0ef05a39ad8680441f9d3a4e17d
nurtenkarakas <you@example.com>
2022-11-03 12:36:52 +0300

    first commit

---
+++ src/A02.java
@@ -0,0 +1,9 @@
+public class A02 {
+
+    public static void main(String[] args) {
+
+        String str1="Java candir";
+        int sayi=123;
+        System.out.println("A02 class");
+    }
+}
---
+++ src/A03.java
@@ -0,0 +1,24 @@
+public class A03 {
+    public static void main(String[] args) {
+
+        /*
+        Bir projeyi ilk defa olusturdugumuzda
+        1- commit olustur
+        2- uzak masauztunde bir repo olustur
+        3- github da istenen son 3 satiri copy yapip
+           projeye past yapip calistirir
+
+           Bir projeyi bir kere uzak masaustu ile iliskilendirince
+           sonra yapacagim her degisiklik icin
+           1- git add.  :yeni yapilan degisiklikleri git e ekler
+           2- git commit -m"istenen aciklama" : degisiklikleri isleyerek versiyon olusturur
+           3- git push :  son versiyonu uzak masaustune yollar
+         */
+
+    }
+
+
+
+
+
+}
